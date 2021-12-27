975b5ff78a42a4962e655484d65f5f8ab8b67553
ririssilvia <ririssilvia1@gmail.com>
2021-12-27 21:15:02 +0700

    initial commit

--- php/src/config.php
+++ php/src/config.php
@@ -3,7 +3,9 @@ define('DB_SERVER', '172.20.0.2');
 define('DB_USER', 'root');
 define('DB_PASS', 'admin');
 define('DB_NAME', 'peminjaman');
- 
+
+$conn=mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
 if (!$conn) {
     die("<script>alert('Gagal tersambung dengan database.')</script>");
 }
